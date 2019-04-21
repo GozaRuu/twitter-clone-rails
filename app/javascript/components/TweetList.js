3cd26fb77fff78cf3c86f666305ea1c83230b925
GozaRuu <kais.sghari@gmail.com>
2019-04-21 15:21:30 +0200

    fix TweetListItem

--- app/javascript/components/TweetList.js
+++ app/javascript/components/TweetList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import TweetListItem from './TweetListItem'
 
 export class TweetList extends Component {
   render() {
     return (
       <div>
         <ul className="collection">
+          <TweetListItem />
+          <TweetListItem />
           <TweetListItem />
         </ul>
       </div>
--- app/javascript/components/TweetListItem.js
+++ app/javascript/components/TweetListItem.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function TweetListItem() {
   return (
-    <div>
+    <React.Fragment>
       <li className="collection-item avatar">
         <i className="material-icons circle">person_pin</i>
         <span className="title">Jon Snow</span>
         <p>Tweet #first</p>
       </li>
-    </div>
+    </React.Fragment>
   )
 }
