a992fc90dd4b4d1cf5c4f16985da69eb1f042352
GozaRuu <kais.sghari@gmail.com>
2019-04-21 14:57:41 +0200

    add tweetbox and refactor main component

---
+++ app/javascript/components/TweetBox.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class TweetBox extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="input-field">
+            <textarea className="materialize-textarea"/>
+            <label>what up</label>
+            <button className="btn right">Tweet</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default TweetBox
---
+++ app/javascript/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TweetBox from './TweetBox'
+
+export class main extends Component {
+  render() {
+    return (
+      <div className="container">
+        <TweetBox />
+      </div>
+    )
+  }
+}
+
+export default main
