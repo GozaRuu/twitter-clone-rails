ad63397fef1c70de80a8f80e02644343187f8746
GozaRuu <kais.sghari@gmail.com>
2019-04-22 16:18:41 +0200

    add gravatar

--- app/javascript/components/TweetListItem.js
+++ app/javascript/components/TweetListItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import moment from 'moment'
 
-export default function TweetListItem({ name, body, created_at }) {
+export default function TweetListItem({ name, body, created_at, gravatar }) {
   return (
     <React.Fragment>
       <li className="collection-item avatar">
-        <i className="material-icons circle">person_pin</i>
+        <img className="circle" src={gravatar} />
         <span className="title">{name}</span>
         <time>{moment(created_at).fromNow()}</time>
         <p>{body}</p>
