80676d0d6fc6b933ed72d95c2606232e7d12906a
GozaRuu <kais.sghari@gmail.com>
2019-04-22 16:07:14 +0200

    add formated date to tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        render json: Tweet.includes(:user).all
+        render json: Tweet.includes(:user).order(created_at: :desc).all
     end
 
     def create
--- app/javascript/components/TweetListItem.js
+++ app/javascript/components/TweetListItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import moment from 'moment'
 
-export default function TweetListItem({ name, body }) {
+export default function TweetListItem({ name, body, created_at }) {
   return (
     <React.Fragment>
       <li className="collection-item avatar">
         <i className="material-icons circle">person_pin</i>
         <span className="title">{name}</span>
+        <time>{moment(created_at).fromNow()}</time>
         <p>{body}</p>
       </li>
     </React.Fragment>
